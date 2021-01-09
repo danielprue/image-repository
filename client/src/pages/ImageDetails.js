114a64281e53c4f5c67f935834098bb11e1d66d0
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2021-01-09 02:21:26 -0700

    images on image details page normalized

--- client/src/pages/ImageDetails.js
+++ client/src/pages/ImageDetails.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 
-import { Button, Descriptions, PageHeader, Tag } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import { Descriptions, PageHeader, Tag } from 'antd';
 import { Image } from 'cloudinary-react';
 
 import '../styling/ImageDetails.css';
 
 const ImageDetails = () => {
   const [image, setImage] = useState(null);
+  const [imageDimensions, setImageDimensions] = useState({});
   const [uploader, setUploader] = useState('unknown');
   const match = useRouteMatch();
   const history = useHistory();
@@ -31,16 +31,19 @@ const ImageDetails = () => {
       .catch((err) => console.log(err));
   };
 
-  // const handleDownloadClick = () => {
-  //   fetch(`http://localhost:3001/api/photos/${image.id}/download`);
-  // };
-
   useEffect(() => {
     fetchImage(match.params.id);
   }, [match.params.id]);
 
   useEffect(() => {
-    if (image) fetchUsername(image.uploader);
+    if (image) {
+      fetchUsername(image.uploader);
+      if (image.width > image.height) {
+        setImageDimensions({ width: '80%' });
+      } else {
+        setImageDimensions({ height: '100%' });
+      }
+    }
   }, [image]);
 
   return (
@@ -51,17 +54,13 @@ const ImageDetails = () => {
           onBack={() => history.goBack()}
           title={image ? image.name : 'Loading'}
           subTitle={image ? `by ${uploader}` : 'loading'}
-          extra={[
-            <a href='' download={image ? image.image_path : ''}>
-              <Button type='primary' icon={<DownloadOutlined />} size='large'>
-                Download
-              </Button>
-            </a>,
-          ]}
         >
           <Descriptions size='small' column={1}>
             <Descriptions.Item label='Description'>
               {image ? image.description : ''}
+            </Descriptions.Item>
+            <Descriptions.Item label='Dimensions'>
+              {image ? `${image.width} x ${image.height}` : ''}
             </Descriptions.Item>
             <Descriptions.Item label='Tags'>
               {image ? (
@@ -79,24 +78,16 @@ const ImageDetails = () => {
       </div>
       {image ? (
         <>
-          <div className='image-display'>
+          <div
+            className='image-display'
+            style={{ maxWidth: '1024px', width: '100%', height: '60vh' }}
+          >
             <Image
               cloudName='devm7fql3'
               publicId={image.public_id}
-              width='512'
+              style={{ ...imageDimensions }}
             />
           </div>
-          {/* <Card title={image.name} className='image-info'>
-            <p>{image.description}</p>
-            <p>{`${image.width} x ${image.height}`}</p>
-            {image.tags.map((value, i) => {
-              return (
-                <Tag key={i} className='image-info-tag'>
-                  {value}
-                </Tag>
-              );
-            })}
-          </Card> */}
         </>
       ) : (
         <></>
